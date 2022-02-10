29d3e89f52a710ac0be34a84e660e787c4a326cb
Ariel Fiuza <arielfiuzaqa@gmail.com>
2022-02-09 21:12:28 -0300

    Commit Segundo 2.0

--- src/test/java/steps/DSL.java
+++ src/test/java/steps/DSL.java
@@ -12,14 +12,9 @@ public class DSL {
     public DSL(WebDriver driver) { this.driver = driver; }
 
     public void abrirBrowser(){ System.setProperty("webdriver.chrome.driver", "C:/Program Files/JetBrains/drivers/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-    }
+        driver = new ChromeDriver(); driver.manage().window().maximize(); }
     public void vaiParaSite(String site){ driver.get(site); }
-    public void preencheCamposDeUsuarioESenha(String campoUsuario, String campoSenha){
-        driver.findElement(By.id("user-name")).sendKeys(campoUsuario);
-        driver.findElement(By.id("password")).sendKeys(campoSenha);
-    }
+    public void preencheCamposDeUsuarioESenha(String campoUsuario, String campoSenha){ driver.findElement(By.id("user-name")).sendKeys(campoUsuario); driver.findElement(By.id("password")).sendKeys(campoSenha); }
     public void click(){ driver.findElement(By.id("login-button")).click(); }
     public void Confirmo(String confirmou){ Assert.assertEquals(confirmou, driver.getCurrentUrl()); System.out.println("O site é:  " + driver.getCurrentUrl()); }
     public void Checar(String resultado){ System.out.println("O teste " + resultado); }
